8b0b21889cc531311ed07331eab53485910e97d4
ho <hohoya33@gmail.com>
2018-05-09 23:49:07 +0900

    Error Boundaries 에러가 발생하면 이를 구분하고 관리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ['Ryan', 'Apeach', 'Frodo', 'Neo']
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render(){
+    const { friends } = this.state;
+    return friends.map(friend => `${friend}`);
+  }
+}
+
+
 class Portals extends Component {
   render(){
     return createPortal(<Message />, document.getElementById('touchme'));
   }
 }
 
-const Message = () => 'Just touched it!'
+const Message = () => 'Just touched it!';
 
 class ReturnType extends Component {
   render() {
@@ -15,12 +33,25 @@ class ReturnType extends Component {
   }
 }
 
+const ErrorFallback = () => 'Sorry something went wrong';
+
 class App extends Component {
+  state = {
+    hasError: false
+  };
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+      hasError: true
+    });
+  };
   render() {
+    const { hasError } = this.state;
     return (
       <Fragment>
         <ReturnType />
         <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
       </Fragment>
     );
   }
